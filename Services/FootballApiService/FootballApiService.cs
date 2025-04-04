ccc7e30b0dc3aadd494f217b46344b633d4ae5ab
davidperjans <perjansdavid@gmail.com>
2025-04-04 11:38:19 +0200

    Implementerat en lista av arenor:
    Har skapat en tabell i databasen för att lagra alla arenor jag hämtar från det externa API-anropet.
    
    Så till framtiden så har jag en egen lista med arenor istället för att skicka en request för varje arena en användare söker efter.
    
    Det är mer effektivt och tar mindre tid samt billigare.

--- Services/FootballApiService/FootballApiService.cs
+++ Services/FootballApiService/FootballApiService.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using AwayDayzAPI.Database;
+using AwayDayzAPI.Models;
 using AwayDayzAPI.Models.DTOs.Stadium;
 using AwayDayzAPI.Models.Responses;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 
 namespace AwayDayzAPI.Services.FootballApiService
@@ -8,12 +11,14 @@ namespace AwayDayzAPI.Services.FootballApiService
     public class FootballApiService : IFootballApiService
     {
         private readonly IMapper _mapper;
+        private readonly AppDbContext _context;
         private readonly HttpClient _client;
         private readonly string _apiKey;
 
-        public FootballApiService(IMapper mapper, IConfiguration configuration)
+        public FootballApiService(IMapper mapper, AppDbContext context, IConfiguration configuration)
         {
             _mapper = mapper;
+            _context = context;
             _client = new HttpClient();
             _apiKey = configuration.GetValue<string>("ApiSettings:FootballApiKey");
             _client.DefaultRequestHeaders.Add("x-apisports-key", _apiKey);
@@ -22,6 +27,16 @@ namespace AwayDayzAPI.Services.FootballApiService
 
         public async Task<List<StadiumDto>> GetStadiumInfoAsync(string stadiumName)
         {
+            // Check if the stadium already exists in the database
+            var existingMatchningStadiums = await _context.Stadiums.Where(stadium => stadium.Name.Contains(stadiumName)).ToListAsync();
+
+            if (existingMatchningStadiums.Any())
+            {
+                var stadiumDto = existingMatchningStadiums.Select(stadium => _mapper.Map<StadiumDto>(stadium)).ToList();
+                return stadiumDto;
+            }
+
+            // If the stadium doesnt exist, call the API
             var response = await _client.GetAsync($"https://v3.football.api-sports.io/venues?search={stadiumName}");
 
             if (!response.IsSuccessStatusCode)
@@ -30,7 +45,29 @@ namespace AwayDayzAPI.Services.FootballApiService
             var content = await response.Content.ReadAsStringAsync();
             var apiResponse = JsonConvert.DeserializeObject<StadiumApiResponse>(content);
 
-            var stadiumDtos = _mapper.Map<List<StadiumDto>>(apiResponse.Response);
+            // List for storing mapped stadium DTOs
+            var stadiumDtos = new List<StadiumDto>();
+
+            foreach (var stadium in apiResponse.Response)
+            {
+                var newStadium = new Stadium
+                {
+                    Name = stadium.Name ?? "Uknown",
+                    Address = stadium.Address ?? "Uknown",
+                    City = stadium.City ?? "Uknown",
+                    Country = stadium.Country ?? "Uknown",
+                    Capacity = stadium.Capacity > 0 ? stadium.Capacity : 0,
+                    Surface = stadium.Surface ?? "Uknown",
+                    ImageUrl = stadium.Image ?? "Uknown"
+                };
+
+                // Add the new stadium to the database
+                _context.Stadiums.Add(newStadium);
+                await _context.SaveChangesAsync();
+
+                // Map the api-response to StadiumDto and add to the list
+                stadiumDtos.Add(_mapper.Map<StadiumDto>(stadium));
+            }
 
             return stadiumDtos;
         }
